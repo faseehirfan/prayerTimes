2aeaaaa6759303ac2ebc3b69378e78c72395c390
Faseeh Irfan <faseeh_irfan@outlook.com>
2024-11-15 20:08:47 -0800

    feat: landing page header

--- prayer-times/app/(landing-page)/layout.tsx
+++ prayer-times/app/(landing-page)/layout.tsx
@@ -1,3 +1,21 @@
+import { LandingPageHeader } from "@/components/landing-page-header";
+
 export default function Layout(props: { children: React.ReactNode }) {
-  return <div className="flex min-h-screen flex-col">hello</div>;
+  return (
+    <div className="flex min-h-screen flex-col">
+      <LandingPageHeader
+        items={[
+          { title: "Home", href: "/" },
+          { title: "Features", href: "/#features" },
+          { title: "Pricing", href: "/#pricing" },
+          {
+            title: "Github",
+            href: "https://github.com/faseehirfan/prayerTimes",
+            external: true,
+          },
+        ]}
+      />
+      <main className="flex-1">{props.children}</main>
+    </div>
+  );
 }
